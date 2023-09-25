49cd6e80e1d799bbecf0f3b7415845f43bb24b99
Ross Towe <ross.towe@shell.com>
2023-09-25 13:38:45 +0100

    Updating license agreement

--- Code/CDFInterp1.m
+++ Code/CDFInterp1.m
@@ -1,4 +1,4 @@
-% Copyright 2023 covXtreme
+% Copyright © [2023] Shell Global Solutions International B.V. All Rights Reserved.
 %
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
